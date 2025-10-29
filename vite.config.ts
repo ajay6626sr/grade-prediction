9eaa2a370b529866d344f90663e7823ddcfd533b
ajay6626sr <163178086+ajay6626sr@users.noreply.github.com>
2025-10-30 01:51:49 +0530

    Fix GitHub Pages 404s in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/grade-prediction/', // 👈 REQUIRED for GitHub Pages
+  base: '/grade-prediction/', // 👈 This line fixes 404s on GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
