0e2fa0b353a9799bbf8fadf8aab4d82e40950401
ajay6626sr <163178086+ajay6626sr@users.noreply.github.com>
2025-10-30 02:17:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/grade-prediction/', // 👈 this must match your repo name
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: './', // ✅ Use relative path for Render
+})
