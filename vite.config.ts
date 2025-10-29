f6702f9b6500e4767ebe2b9c77542d0c05e9a2be
ajay6626sr <163178086+ajay6626sr@users.noreply.github.com>
2025-10-30 02:45:53 +0530

    Remove unnecessary comments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ✅ CHANGE THIS to your actual GitHub repo name!
+// 👇 Change "your-repo-name" to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/grade-prediction/', // Example: '/grade-prediction/'
-  build: {
-    outDir: 'dist',
-  },
+  base: '/grade-prediction/', // example: '/grade-prediction/'
 });
